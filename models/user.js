9eb5443adbc1b11a40410e116d2719aec0058abf
clairecleverlamb <clairechenszeying@gmail.com>
2025-03-12 23:41:35 -0700

    wip: set up env and build schema

--- models/user.js
+++ models/user.js
@@ -1,5 +1,30 @@
 const mongoose = require('mongoose');
 
+const applicationSchema = mongoose.Schema({
+  college: {
+    type: String,
+    required: true,
+  },
+  major: {
+    type: String,
+    required: true,
+  },
+  deadline: {
+    type: Date,
+    required: true,
+  },
+  notes: {
+    type: String,
+  },
+  postingLink: {
+    type: String,
+  },
+  status: {
+    type: String,
+    enum: ['interested', 'applied', 'interviewing', 'rejected', 'accepted'],
+  }
+})
+
 const userSchema = mongoose.Schema({
   username: {
     type: String,
@@ -9,6 +34,7 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  applications: [applicationSchema],
 });
 
 const User = mongoose.model('User', userSchema);
